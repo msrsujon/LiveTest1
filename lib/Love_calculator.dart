e9ff778ea672df441e9f9e8becdb0fa50204d994
msrsujon <msrsujon12@gmail.com>
2024-10-05 10:12:09 +0600

    love calculation in flutter

--- lib/Love_calculator.dart
+++ lib/Love_calculator.dart
@@ -1,8 +1,8 @@
+import 'dart:math';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:project1/flutterproject1.dart';
 
-main() {
+void main() {
   runApp(const MyApp());
 }
 
@@ -12,22 +12,179 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: ThemeData(primarySwatch: Colors.green),
-        darkTheme: ThemeData(primarySwatch: Colors.grey),
-        debugShowCheckedModeBanner: false,
-        // ignore: prefer_const_constructors
-        home: HomeActivity());
+      theme: ThemeData(primarySwatch: Colors.green),
+      darkTheme: ThemeData(primarySwatch: Colors.grey),
+      debugShowCheckedModeBanner: false,
+      home: const HomeActivity(),
+    );
   }
 }
 
-class HomeActivity extends StatelessWidget {
+class HomeActivity extends StatefulWidget {
   const HomeActivity({super.key});
+
+  @override
+  _HomeActivityState createState() => _HomeActivityState();
+}
+
+class _HomeActivityState extends State<HomeActivity> {
+  final TextEditingController _textInputController1 = TextEditingController();
+  final TextEditingController _textInputController2 = TextEditingController();
+
+  bool widgetVisible = false;
+  String resultText = '';
+
+  void calculateLovePercentage() {
+    setState(() {
+      widgetVisible = true;
+      Random random = Random();
+      int randomNumber =
+          random.nextInt(51) + 50; // Generates a number between 50 and 100
+      resultText = '$randomNumber%';
+    });
+  }
+
+  void tryAgain() {
+    setState(() {
+      widgetVisible = false;
+      _textInputController1.clear();
+      _textInputController2.clear();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Love❤ Calculator❤'),
+        title: const Text(
+          'Love❤ Calculator❤',
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.red,
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Stack(
+            children: [
+              Image.asset(
+                'assets/images/lovee.jpg',
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                fit: BoxFit.cover,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(30),
+                child: Column(
+                  children: [
+                    const SizedBox(height: 50),
+                    TextField(
+                      decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Colors.red.shade900, width: 2.0),
+                          borderRadius: BorderRadius.circular(50.0),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Colors.red.shade900, width: 2.0),
+                          borderRadius: BorderRadius.circular(50.0),
+                        ),
+                        hintText: 'Enter Your Name',
+                        hintStyle: TextStyle(
+                            color: Colors.red.shade900,
+                            fontWeight: FontWeight.w400),
+                      ),
+                      controller: _textInputController1,
+                    ),
+                    const SizedBox(height: 0),
+                    const Icon(
+                      Icons.favorite,
+                      size: 120,
+                      color: Colors.red,
+                    ),
+                    const SizedBox(height: 10),
+                    TextField(
+                      decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Colors.red.shade900, width: 2.0),
+                          borderRadius: BorderRadius.circular(50.0),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Colors.red.shade900, width: 2.0),
+                          borderRadius: BorderRadius.circular(50.0),
+                        ),
+                        hintText: 'Enter Your Crush Name',
+                        hintStyle: TextStyle(
+                            color: Colors.red.shade900,
+                            fontWeight: FontWeight.w500),
+                      ),
+                      controller: _textInputController2,
+                    ),
+                    const SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        TextButton(
+                          style: TextButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15.0)),
+                            foregroundColor: Colors.white,
+                            backgroundColor: Colors.pink.shade400,
+                            padding: const EdgeInsets.all(16.0),
+                            textStyle: const TextStyle(fontSize: 17),
+                          ),
+                          onPressed: calculateLovePercentage,
+                          child: const Text('Calculate'),
+                        ),
+                        TextButton(
+                          style: TextButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15.0)),
+                            foregroundColor: Colors.white,
+                            backgroundColor: Colors.pink.shade400,
+                            padding: const EdgeInsets.all(16.0),
+                            textStyle: const TextStyle(fontSize: 17),
+                          ),
+                          onPressed: tryAgain,
+                          child: const Text('Try Again'),
+                        ),
+                      ],
+                    ),
+                    Visibility(
+                      visible: widgetVisible,
+                      child: Stack(
+                        children: [
+                          const Positioned(
+                            child: Icon(
+                              Icons.favorite,
+                              size: 255,
+                              color: Colors.red,
+                            ),
+                          ),
+                          Positioned(
+                            top: 90,
+                            left: 75,
+                            child: Text(
+                              resultText,
+                              style: const TextStyle(
+                                fontSize: 45,
+                                fontWeight: FontWeight.w600,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,119 +9,183 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      theme: ThemeData(primarySwatch: Colors.green),
+      darkTheme: ThemeData(primarySwatch: Colors.grey),
+      debugShowCheckedModeBanner: false,
+      home: const HomeActivity(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class HomeActivity extends StatefulWidget {
+  const HomeActivity({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _HomeActivityState createState() => _HomeActivityState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomeActivityState extends State<HomeActivity> {
+  final TextEditingController _textInputController1 = TextEditingController();
+  final TextEditingController _textInputController2 = TextEditingController();
 
-  void _incrementCounter() {
+  bool widgetVisible = false;
+  String resultText = '';
+
+  void calculateLovePercentage() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      widgetVisible = true;
+      Random random = Random();
+      int randomNumber =
+          random.nextInt(51) + 50; // Generates a number between 50 and 100
+      resultText = '$randomNumber%';
+    });
+  }
+
+  void tryAgain() {
+    setState(() {
+      widgetVisible = false;
+      _textInputController1.clear();
+      _textInputController2.clear();
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text(
+          'Love❤ Calculator❤',
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.red,
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        child: SingleChildScrollView(
+          child: Stack(
+            children: [
+              Image.asset(
+                'assets/images/lovee.jpg',
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                fit: BoxFit.cover,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(30),
+                child: Column(
+                  children: [
+                    const SizedBox(height: 10),
+                    TextField(
+                      decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Colors.red.shade900, width: 2.0),
+                          borderRadius: BorderRadius.circular(50.0),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Colors.red.shade900, width: 2.0),
+                          borderRadius: BorderRadius.circular(50.0),
+                        ),
+                        hintText: 'Enter Your Name',
+                        hintStyle: TextStyle(
+                            color: Colors.red.shade900,
+                            fontWeight: FontWeight.w500),
+                      ),
+                      controller: _textInputController1,
+                    ),
+                    const SizedBox(height: 10),
+                    const Icon(
+                      Icons.favorite,
+                      size: 60,
+                      color: Colors.red,
+                    ),
+                    const SizedBox(height: 10),
+                    TextField(
+                      decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Colors.red.shade900, width: 2.0),
+                          borderRadius: BorderRadius.circular(50.0),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Colors.red.shade900, width: 2.0),
+                          borderRadius: BorderRadius.circular(50.0),
+                        ),
+                        hintText: 'Enter Your Crush Name',
+                        hintStyle: TextStyle(
+                            color: Colors.red.shade900,
+                            fontWeight: FontWeight.w500),
+                      ),
+                      controller: _textInputController2,
+                    ),
+                    const SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        TextButton(
+                          style: TextButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15.0)),
+                            foregroundColor: Colors.white,
+                            backgroundColor: Colors.pink.shade400,
+                            padding: const EdgeInsets.all(16.0),
+                            textStyle: const TextStyle(fontSize: 17),
+                          ),
+                          onPressed: calculateLovePercentage,
+                          child: const Text('Calculate'),
+                        ),
+                        TextButton(
+                          style: TextButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15.0)),
+                            foregroundColor: Colors.white,
+                            backgroundColor: Colors.pink.shade400,
+                            padding: const EdgeInsets.all(16.0),
+                            textStyle: const TextStyle(fontSize: 17),
+                          ),
+                          onPressed: tryAgain,
+                          child: const Text('Try Again'),
+                        ),
+                      ],
+                    ),
+                    Visibility(
+                      visible: widgetVisible,
+                      child: Stack(
+                        children: [
+                          const Positioned(
+                            child: Icon(
+                              Icons.favorite,
+                              size: 155,
+                              color: Colors.red,
+                            ),
+                          ),
+                          Positioned(
+                            top: 30,
+                            left: 40,
+                            child: Text(
+                              resultText,
+                              style: const TextStyle(
+                                fontSize: 45,
+                                fontWeight: FontWeight.w600,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
